424625afaf6d30f7039322cf72e51a05d5bb04d7
xaatim <xayari229@gmail.com>
2026-06-10 01:02:26 +0300

    refactor ArmTest to use Cartesian goals with TRAC-IK and update logging messages

--- src/robot_vision/robot_vision/arm_test_node.py
+++ src/robot_vision/robot_vision/arm_test_node.py
@@ -3,7 +3,9 @@ from rclpy.node import Node
 from rclpy.action import ActionClient
 from rclpy.parameter import Parameter
 from moveit_msgs.action import MoveGroup
-from moveit_msgs.msg import MotionPlanRequest, Constraints, JointConstraint
+from moveit_msgs.msg import MotionPlanRequest, Constraints, PositionConstraint, BoundingVolume
+from geometry_msgs.msg import PoseStamped
+from shape_msgs.msg import SolidPrimitive
 
 class ArmTest(Node):
     def __init__(self):
@@ -12,7 +14,7 @@ class ArmTest(Node):
         self.client = ActionClient(self, MoveGroup, 'move_action')
         self.goal_sent = False
         self.timer = self.create_timer(3.0, self.fire_goal)
-        self.get_logger().info('Arm test spinning up... sending goal in 3 secs.')
+        self.get_logger().info('Arm test spinning up... sending Cartesian goal in 3 secs.')
 
     def fire_goal(self):
         if self.goal_sent:
@@ -23,31 +25,43 @@ class ArmTest(Node):
         self.get_logger().info('Hunting for move_action server...')
         self.client.wait_for_server()
 
-        # Build joint constraints instead of XYZ Cartesian constraints
-        goal_constraints = Constraints()
-        
-        # A simple, safe pose
-        joint_names = [
-            'base_yaw_joint', 
-            'shoulder_pitch_joint', 
-            'elbow_pitch_joint', 
-            'wrist_pitch_joint', 
-            'wrist_roll_joint'
-        ]
-        target_angles = [0.5, -0.4, 0.5, 0.0, 0.0] 
+        # Cartesian XYZ Target (Simulating a YOLOE detection coordinate)
+        target_x = 0.4
+        target_y = 0.0
+        target_z = 0.5
 
-        for i in range(len(joint_names)):
-            jc = JointConstraint()
-            jc.joint_name = joint_names[i]
-            jc.position = target_angles[i]
-            jc.tolerance_above = 0.05
-            jc.tolerance_below = 0.05
-            jc.weight = 1.0
-            goal_constraints.joint_constraints.append(jc)
+        pose = PoseStamped()
+        pose.header.frame_id = 'base_link'
+        pose.header.stamp = self.get_clock().now().to_msg()
+        pose.pose.position.x = target_x
+        pose.pose.position.y = target_y
+        pose.pose.position.z = target_z
+        pose.pose.orientation.w = 1.0
+
+        # We give the solver a 5cm sphere to aim for
+        sphere = SolidPrimitive()
+        sphere.type = SolidPrimitive.SPHERE
+        sphere.dimensions = [0.05]
+
+        vol = BoundingVolume()
+        vol.primitives.append(sphere) # type: ignore
+        vol.primitive_poses.append(pose.pose) # type: ignore
+
+        # Tie the constraint to your camera link
+        constraint = PositionConstraint()
+        constraint.header.frame_id = 'base_link'
+        constraint.link_name = 'camera_link'
+        constraint.constraint_region = vol
+        constraint.weight = 1.0
+
+        goal_constraints = Constraints()
+        goal_constraints.position_constraints.append(constraint) # type: ignore
 
         req = MotionPlanRequest()
         req.group_name = 'arm'
-        req.goal_constraints.append(goal_constraints)
+        req.goal_constraints.append(goal_constraints) # type: ignore
+        
+        # Give TRAC-IK plenty of attempts and time to solve the math
         req.num_planning_attempts = 10
         req.allowed_planning_time = 5.0
         req.max_velocity_scaling_factor = 0.3
@@ -59,7 +73,7 @@ class ArmTest(Node):
         goal_msg.planning_options.replan = True
         goal_msg.planning_options.replan_attempts = 3
 
-        self.get_logger().info('Sending Joint-Space goal to bypass IK...')
+        self.get_logger().info(f'Sending Cartesian goal to X:{target_x} Y:{target_y} Z:{target_z}')
         
         self.future = self.client.send_goal_async(goal_msg)
         self.future.add_done_callback(self.goal_response_cb)
@@ -77,9 +91,9 @@ class ArmTest(Node):
     def result_cb(self, future):
         status = future.result().result.error_code.val
         if status == 1:
-            self.get_logger().info('SUCCESS! Target reached.')
+            self.get_logger().info('SUCCESS! TRAC-IK solved the Cartesian target.')
         else:
-            self.get_logger().error(f'FAILED. Error code: {status}. Try tweaking the target angles.')
+            self.get_logger().error(f'FAILED. Error code: {status}. Try tweaking the XYZ target.')
 
 def main(args=None):
     rclpy.init(args=args)
