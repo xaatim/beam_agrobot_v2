d4c384f7bc117fae110a2e430e19cc77d24b40d9
xaatim <xayari229@gmail.com>
2026-06-29 06:07:19 +0300

    Updated navigation launch file to use correct map and parameters

--- src/robot_navigation/launch/nav2.launch.py
+++ src/robot_navigation/launch/nav2.launch.py
@@ -9,25 +9,23 @@ def generate_launch_description():
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     my_nav_dir = get_package_share_directory('robot_navigation')
 
-    # Define your exact map path
-    map_file = os.path.join(my_nav_dir, 'maps', 'agrobot_field.yaml')
+    map_file = os.path.join(my_nav_dir, 'maps', 'slam_agrobot.yaml')
+    nav2_params = os.path.join(my_nav_dir, 'config', 'nav2_params.yaml')
 
-    # GroupAction applies the remap to EVERYTHING launched inside it
     nav2_group = GroupAction(
         actions=[
-            # This wires the Nav2 brain directly to your Gazebo wheels
             SetRemap(src='/cmd_vel', dst='/diff_cont/cmd_vel_unstamped'),
-            
-            # Launch Nav2 (without the buggy params file)
             IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')),
+                PythonLaunchDescriptionSource(
+                    os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
+                ),
                 launch_arguments={
                     'map': map_file,
+                    'params_file': nav2_params,
                     'use_sim_time': 'true',
                     'autostart': 'true'
                 }.items()
             )
         ]
     )
-
     return LaunchDescription([nav2_group])
